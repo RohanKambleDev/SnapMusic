ee2782a61cd1badeedc84b43e1814208724d93c7
rohanatveeva <rohan.kamble@veeva.com>
2024-11-23 02:25:20 +0530

    1st commit - working

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UploadController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,4 +29,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/', [UploadController::class, 'showUploadForm']);
+Route::post('/upload', [UploadController::class, 'handleUpload']);
+
+require __DIR__ . '/auth.php';
